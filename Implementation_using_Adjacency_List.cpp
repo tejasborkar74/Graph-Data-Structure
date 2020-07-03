ff8e7765683bc686b410692cd385d266c40d4e41
unknown <tejasborkar74@gmail.com>
2020-07-03 17:39:57 +0530

    Implementation

---
+++ Implementation_using_Adjacency_List.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class graph
+{
+    map<int,list<int> > adjList;
+
+public:
+    graph()
+    {    }
+
+    void addEdge(int u,int v,bool isbidi=true)
+    {
+        adjList[u].push_back(v);
+
+        if(isbidi)
+        {
+            adjList[v].push_back(u);
+        }
+    }
+
+    void printGraph()
+    {
+        for(int i : adjList)
+        {
+            cout<<i.first()<<"-->";
+            for(int neig : adjList[i])
+            {
+                cout<<neig<<" ,";
+            }
+
+            cout<<endl;
+        }
+    }
+};
+
+int main()
+{
+    graph g;
+
+    g.addEdge(0,1);
+    g.addEdge(0,4);
+    g.addEdge(3,1);
+    g.addEdge(3,4);
+    g.addEdge(4,1);
+    g.addEdge(2,3);
+    g.addEdge(2,1);
+
+    g.printGraph();
+}
