b19d2026bc761788fe00f27ff9e6222619fd4728
unknown <tejasborkar74@gmail.com>
2020-07-03 17:57:48 +0530

    BFS

---
+++ BFS.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class graph
+{
+    map<int,list<int> > adjList;
+
+public:
+    void addEdge(int u,int v,bool isbi=true)
+    {
+        adjList[u].push_back(v);
+
+        if(isbi)
+        {
+            adjList[v].push_back(u);
+        }
+    }
+
+    void BFS(int src)
+    {
+        queue<int> q;
+        q.push(src);
+        q.push(-1);
+
+        map<int,bool> visited;
+        visited[src]=true;
+
+        while(!q.empty())
+        {
+            int f = q.front();
+            if(f==-1)
+            {
+                cout<<endl;
+                q.pop();
+                if(q.empty())
+                {
+                    break;
+                }
+                q.push(-1);
+            }
+
+            f=q.front();
+
+            q.pop();
+
+            cout<<f<<" ";
+
+            for(auto neigh : adjList[f])
+            {
+                if(visited[neigh]==false)
+                {
+                    q.push(neigh);
+                    visited[neigh]=true;
+                }
+            }
+        }
+    }
+
+};
+int main()
+{
+     graph g;
+
+    g.addEdge(0,1);
+    g.addEdge(0,4);
+    g.addEdge(3,1);
+    g.addEdge(3,4);
+    g.addEdge(4,1);
+    g.addEdge(2,3);
+    g.addEdge(2,1);
+
+    g.BFS(0);
+
+}
